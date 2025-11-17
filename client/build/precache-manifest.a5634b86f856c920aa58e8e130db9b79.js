747f78757791bd5998bb8f4129c1f6b5d45ed045
zaidfadel1989 <zaid.fadel89@gmail.com>
2025-11-16 21:03:53 -0600

    deploying

---
+++ client/build/precache-manifest.a5634b86f856c920aa58e8e130db9b79.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1946608f386127b0f49665137922ffbe",
+    "url": "/ChangeTheWord/index.html"
+  },
+  {
+    "revision": "d8de6fd8f0e767381e91",
+    "url": "/ChangeTheWord/static/css/2.3b956902.chunk.css"
+  },
+  {
+    "revision": "26f14a6a357d68ee28d3",
+    "url": "/ChangeTheWord/static/css/main.0fb15a8d.chunk.css"
+  },
+  {
+    "revision": "d8de6fd8f0e767381e91",
+    "url": "/ChangeTheWord/static/js/2.4c68f573.chunk.js"
+  },
+  {
+    "revision": "b4e9cf07265476c4a068423ce0546678",
+    "url": "/ChangeTheWord/static/js/2.4c68f573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f14a6a357d68ee28d3",
+    "url": "/ChangeTheWord/static/js/main.68a302a9.chunk.js"
+  },
+  {
+    "revision": "7bda74762d4b3feba78a34074b73d123",
+    "url": "/ChangeTheWord/static/js/main.68a302a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c6b15f0b102cb43bf0",
+    "url": "/ChangeTheWord/static/js/runtime-main.caa5bb37.js"
+  },
+  {
+    "revision": "e0ba28fa20ce473fa32aed6fd5a612db",
+    "url": "/ChangeTheWord/static/media/Buy.e0ba28fa.png"
+  },
+  {
+    "revision": "aadd38193598ccd833d2345193856e41",
+    "url": "/ChangeTheWord/static/media/Footer-City.aadd3819.png"
+  },
+  {
+    "revision": "7b7b9591191968b0f4625201d100301a",
+    "url": "/ChangeTheWord/static/media/Rent.7b7b9591.png"
+  },
+  {
+    "revision": "3578f0d80f6edd1b80ebb5be61b00744",
+    "url": "/ChangeTheWord/static/media/Sell.3578f0d8.png"
+  },
+  {
+    "revision": "2631cd03e9aa90a5b21f503a9d1a4ab5",
+    "url": "/ChangeTheWord/static/media/back.2631cd03.jpg"
+  },
+  {
+    "revision": "8d5810fccbb6ffce6311a73fbfe16f6b",
+    "url": "/ChangeTheWord/static/media/chi.8d5810fc.jpg"
+  },
+  {
+    "revision": "b2759c44a4e8c569a0fd53821b502be7",
+    "url": "/ChangeTheWord/static/media/chicago-log.b2759c44.jpg"
+  },
+  {
+    "revision": "76ffe8715f5c90e17693224f5ccb5baf",
+    "url": "/ChangeTheWord/static/media/lognav.76ffe871.png"
+  },
+  {
+    "revision": "aaf8ab5dccafc282d3d3d9820f24c502",
+    "url": "/ChangeTheWord/static/media/paper-1990111.aaf8ab5d.jpg"
+  },
+  {
+    "revision": "bfc071bf1bffc9c2483467f007f305e9",
+    "url": "/ChangeTheWord/static/media/sea-s.bfc071bf.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7db9d1fbca0a15ac20b576da6d74e96c.js"
+  "/ChangeTheWord/precache-manifest.a5634b86f856c920aa58e8e130db9b79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChangeTheWord/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
